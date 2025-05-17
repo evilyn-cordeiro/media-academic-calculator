b93f9e50a6926dd7c7a8aaa3860e1550362ccfb3
evilyn-cordeiro <evilyndeveloper@gmail.com>
2025-05-16 21:42:40 -0300

    Add modal improvements, GIFs, and visual enhancements
    Introduces dynamic GIFs based on results to enhance user feedback.
    Redesigns modal styles with conditional formatting for different outcomes
    and improves overall UI/UX with updated visuals and layout adjustments.
    
    Updates footer information to reflect the current year and optimizes
    background and container styles for better responsiveness and readability.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const resultDiv = document.getElementById("result");
   const modal = document.getElementById("modal");
   const closeModal = document.getElementById("close");
+  const gifElement = document.getElementById("gif");
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -15,6 +16,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const tde3 = parseFloat(document.getElementById("tde3").value) || 0;
     const tde4 = parseFloat(document.getElementById("tde4").value) || 0;
 
+    let srcGif = "";
+
     // Calcular a média com base na fórmula fornecida
     const mediaFinal =
       0.4 * avp1 + 0.4 * avp2 + 0.05 * (tde1 + tde2 + tde3 + tde4);
@@ -26,16 +29,24 @@ document.addEventListener("DOMContentLoaded", function () {
     let situacao = "";
     if (mediaFinalArredondada >= 7) {
       situacao = "Aprovado";
+      srcGif =
+        "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExbjFkNTl3djdkZzhjbWVodjVheXczOTJob3JlbHl2OHVlZGJ0aXZucyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/pWO49XP9L7TxbgQVib/giphy.gif";
     } else if (mediaFinalArredondada >= 4) {
       situacao = "Avaliação Final (AVF)";
+      srcGif =
+        "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTU0ZjdicDRzbzQ1NGRpYm16OGhoOXQ4azRjbnpnanB4Z3owaHZjMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xSM46ernAUN3y/giphy.gif";
     } else {
       situacao = "Reprovado";
+      srcGif =
+        "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjdwdXJzM3ByaXA0NHVsYWx0cnFvNDA5N3pxdzB3a3I2bjdnZG1rOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/otP85UTRethni/giphy.gif";
     }
 
     // Exibir o resultado no modal
     resultDiv.textContent = `Média Final: ${mediaFinalArredondada.toFixed(
       2
     )} - Situação: ${situacao}`;
+
+    gifElement.src = srcGif;
 
     // Exibir o modal
     modal.style.display = "block";
